7fa0f4c593658539f7895e936d09384d0d3c7ea3
Adam Duflo <adduflo@gmail.com>
2022-03-06 19:25:16 -0500

    restructured Area and made remaining attributes public

--- Sources/WCTRPCommon/Models/Area.swift
+++ Sources/WCTRPCommon/Models/Area.swift
@@ -8,30 +8,20 @@
 import Foundation
 
 public struct Area: Codable {
-    let id: String
-    let name: String
-    let coordinate: Coordinate
-    let zone: Zone
+    public let id: String
+    public let name: String
+    public let coordinate: Coordinate
+    public let zone: Zone
+    
+    public struct Coordinate: Codable {
+        let latitude: String
+        let longitude: String
+    }
     
     public enum Zone: String, CaseIterable, Codable {
         case eastern
         case central
         case mountain
         case pacific
-    }
-    
-    init(id: String,
-         name: String,
-         coordinate: Coordinate,
-         zone: Zone) {
-        self.id = id
-        self.name = name
-        self.coordinate = coordinate
-        self.zone = zone
-    }
-    
-    public struct Coordinate: Codable {
-        let latitude: String
-        let longitude: String
     }
 }
