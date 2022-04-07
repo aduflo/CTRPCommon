5aa07eed8dfb62056ca87509d38b81fed334f689
Adam Duflo <adduflo@gmail.com>
2022-04-06 23:20:10 -0400

    stubbing out AreasByIdResponse structure

--- Sources/CTRPCommon/Models/Responses/AreasByIdResponse.swift
+++ Sources/CTRPCommon/Models/Responses/AreasByIdResponse.swift
@@ -8,12 +8,66 @@
 import Foundation
 
 public struct AreasByIdResponse: Codable {
-    public let id: UUID
-    public let jsonString: String
+    public let metadata: Metadata
+    public let weather: Weather
     
-    public init(id: UUID,
-                jsonString: String) {
-        self.id = id
-        self.jsonString = jsonString
+    public init(metadata: Metadata,
+                weather: Weather) {
+        self.metadata = metadata
+        self.weather = weather
     }
+}
+
+public extension AreasByIdResponse {
+    typealias Metadata = Area
+
+    struct Weather: Codable {
+        let today: Today
+        let recentHistory: RecentHistory
+    }
+
+    struct Today: Codable {
+        let temperatures: Temperatures
+        let daytime: DayUnit
+        let nighttime: DayUnit
+
+        struct Temperatures: Codable {
+            let high: ValueUnit
+            let low: ValueUnit
+        }
+
+        struct DayUnit: Codable {
+            let message: String
+            let percipitation: Percipitation
+
+            struct Percipitation: Codable {
+                let probability: Int
+                let type: PercipitationType?
+                let intensity: Intensity?
+                let amount: ValueUnit
+
+                enum PercipitationType: String, Codable {
+                    case rain
+                    case ice
+                    case snow
+                    case mixed
+                }
+
+                enum Intensity: String, Codable {
+                    case light
+                    case moderate
+                    case heavy
+                }
+            }
+        }
+    }
+
+    struct RecentHistory: Codable {
+
+    }
+}
+
+public struct ValueUnit: Codable {
+    public let value: Double
+    public let unit: String
 }
