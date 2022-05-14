cdb63a683af4999fdc1dafa152ade5efaddb5d22
Adam Duflo <adduflo@gmail.com>
2022-05-14 17:45:26 -0400

    restructured AreasByIdResponse

--- Sources/CTRPCommon/Models/Responses/AreasByIdResponse.swift
+++ Sources/CTRPCommon/Models/Responses/AreasByIdResponse.swift
@@ -9,31 +9,23 @@ import Foundation
 
 public struct AreasByIdResponse: Codable {
     public let metadata: Metadata
-    public let context: Context
+    public let canTheRocksPlay: Bool
+    public let daysSincePrecipitation: Int?
     public let weather: Weather
     
     public init(metadata: Metadata,
-                context: Context,
+                canTheRocksPlay: Bool,
+                daysSincePrecipitation: Int?,
                 weather: Weather) {
         self.metadata = metadata
-        self.context = context
+        self.canTheRocksPlay = canTheRocksPlay
+        self.daysSincePrecipitation = daysSincePrecipitation
         self.weather = weather
     }
 }
 
 public extension AreasByIdResponse {
     typealias Metadata = Area
-
-    struct Context: Codable {
-        public let canTheRocksPlay: Bool
-        public let daysSincePrecipitation: Int?
-
-        public init(canTheRocksPlay: Bool,
-                    daysSincePrecipitation: Int?) {
-            self.canTheRocksPlay = canTheRocksPlay
-            self.daysSincePrecipitation = daysSincePrecipitation
-        }
-    }
 
     struct Weather: Codable {
         public let today: Today
